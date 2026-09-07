34d72b82a362a917204d8c1eb641d54e30dabef4
DTINH <139965711+tinhtinh1908@users.noreply.github.com>
2026-09-08 00:18:00 +0700

    build: use AGP 9 built-in Kotlin support

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.agp.app) apply false
     alias(libs.plugins.agp.lib) apply false
-    alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
 
