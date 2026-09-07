6b4a0b780cb6d9d6a8f337cb8286cd23897f2c45
DTINH <139965711+tinhtinh1908@users.noreply.github.com>
2026-09-07 23:51:57 +0700

    build: compile manager against Android 37

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -29,8 +29,7 @@ cmaker {
 
 val androidMinSdkVersion = 26
 val androidTargetSdkVersion = 36
-val androidCompileSdkVersion = 36
-val androidBuildToolsVersion = "36.1.0"
+val androidCompileSdkVersion = 37
 val androidCompileNdkVersion by extra(libs.versions.ndk.get())
 val androidSourceCompatibility = JavaVersion.VERSION_21
 val androidTargetCompatibility = JavaVersion.VERSION_21
