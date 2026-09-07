aee7df2ed954a3e26a50a19d9bcbb8b9b324cd2f
DTINH <139965711+tinhtinh1908@users.noreply.github.com>
2026-09-08 00:28:37 +0700

    build: restore Kotlin Android plugin for AGP 8

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.agp.app) apply false
     alias(libs.plugins.agp.lib) apply false
+    alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
 
